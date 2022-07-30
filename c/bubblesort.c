b2fff7fc76fb49b6bf434eba92cd561c39e83e2c
satorusxm <sakshamakagautam@gmail.com>
2022-07-30 17:26:50 +0530

    bubble sort

---
+++ c/bubblesort.c
@@ -0,0 +1,35 @@
+// Bubble Sort
+
+#include <stdio.h>
+
+// Sorts an array using bubble sort
+void sort(int *arr, int arrLen);
+
+int main() {
+    int arr[] = {20, 19, 18, 17, 16};
+
+    sort(arr, 5);
+    for (int i = 0; i < 5; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
+void sort(int *arr, int arrLen) {
+    int sorted = 0, temp;
+
+    while (sorted != 1) {
+        sorted = 1;
+        for (int i = 0; i < arrLen - 1; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                sorted = 0;
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+
+    return;
+}
